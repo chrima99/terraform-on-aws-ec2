36cd1a25b4a3cf59340b7b6e7bee2978a41e56a7
Martin Christello <martin.christello@gmail.com>
2026-07-02 16:03:53 -0500

    ec2 name change

--- 02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
+++ 02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
@@ -15,7 +15,7 @@ provider "aws" {
 }
 
 # Resource Block
-resource "aws_instance" "ec2demo" {
+resource "aws_instance" "martino" {
   ami           = "ami-0533f2ba8a1995cf9" # Amazon Linux in us-east-1, update as per your region
   instance_type = "t2.micro"
 }
