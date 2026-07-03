a8360ae933871f54913c7a8fc09ebebd51d2284c
Martin Christello <martin.christello@gmail.com>
2026-07-03 14:52:46 -0500

    uptdate ec2 instance type to t3.micro

--- 02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
+++ 02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
@@ -17,5 +17,5 @@ provider "aws" {
 # Resource Block
 resource "aws_instance" "martino" {
   ami           = "ami-0533f2ba8a1995cf9" # Amazon Linux in us-east-1, update as per your region
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 }
